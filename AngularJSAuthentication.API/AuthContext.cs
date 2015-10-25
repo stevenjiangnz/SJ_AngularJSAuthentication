48f216e3d0a5e9c4e011c730fff8ec4eb95cf5a6
Steven <sj@jnetsolution.com.au>
2015-10-25 13:48:40 +1000

    1

--- AngularJSAuthentication.API/AuthContext.cs
+++ AngularJSAuthentication.API/AuthContext.cs
@@ -4,16 +4,21 @@ using System.Linq;
 using System.Web;
 
 using Microsoft.AspNet.Identity.EntityFramework;
-
+using System.Data.Entity;
+using AngularJSAuthentication.API.Entities;
 
 namespace AngularJSAuthentication.API
 {
     public class AuthContext : IdentityDbContext<IdentityUser>
     {
+
         public AuthContext()
             : base("AuthContext")
         {
 
         }
+
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
     }
 }
--- AngularJSAuthentication.API/Entities/Client.cs
+++ AngularJSAuthentication.API/Entities/Client.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace AngularJSAuthentication.API
+namespace AngularJSAuthentication.API.Entities
 {
-    public class UserModel
+    public class Client
     {
     }
 }
